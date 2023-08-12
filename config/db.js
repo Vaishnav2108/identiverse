70f2d69a51ee1d44e2880da7af3a493270f5547b
surya pratap singh <surya2pratap0singh04@gmail.com>
2023-08-13 02:01:08 +0530

    third commit

---
+++ config/db.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose');
+
+
+const connection = mongoose.createConnection('mongodb://127.0.0.1:27017').on('open',()=>{
+    console.log("MongoDb Connected");
+}).on('error',()=>{
+    console.log("MongoDb connection error");
+});
+
+
+module.exports = connection;
---
+++ routers/user.router.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+const UserController = require("../controller/user.controller");
+
+router.post('/registration',UserController.register);
+
+module.exports = router;
